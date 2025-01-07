b8bb47d1e056962cd54b87a3361aebbec363cacd
duchangkim <duchang.dev@gmail.com>
2025-01-07 15:19:35 +0900

    :sparkles: Add database support for Spring Boot project

---
+++ src/main/kotlin/demo/MessageService.kt
@@ -0,0 +1,26 @@
+package demo
+
+import org.springframework.stereotype.Service
+import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.jdbc.core.query
+import java.util.UUID
+
+@Service
+class MessageService(private val db: JdbcTemplate) {
+    fun findMessages(): List<Message> = db.query("select * from messages") { response, _ ->
+        Message(response.getString("id"), response.getString("text"))
+    }
+
+    fun findMessageById(id: String): Message? = db.query("SELECT * FROM messages WHERE id = ?", id) { response, _ ->
+        Message(response.getString("id"), response.getString("text"))
+    }.singleOrNull()
+
+    fun save(message: Message): Message {
+        val id = message.id ?: UUID.randomUUID().toString()
+        db.update(
+            "insert into messages (id, text) values ( ?, ? )",
+            id, message.text
+        )
+        return message.copy(id = id);
+    }
+}
